c9dd83fb5f23d2aa5da39121f9c1739eb914a933
kaiquepirani <kaiquepirani@gmail.com>
2026-04-24 17:30:24 -0300

    Create route.ts

---
+++ app/api/contratos/importar-completo/route.ts
@@ -0,0 +1,40 @@
+{
+  "contemAditamentos": true,
+  "contrato": {
+    "numero": "007/2024",
+    "contratante": "Prefeitura Municipal de Aguaí",
+    "cnpjContratante": "46.425.229/0001-79",
+    "cidade": "Aguaí",
+    "tipoServico": "Transporte Escolar",
+    "dataInicio": "2024-03-05",
+    "dataVencimento": "2025-03-05",
+    "valorTotal": 1700111.70,
+    "itens": [
+      { "descricao": "Rota 01", "quantidade": 23730, "unidade": "km", "valorUnitario": 7.11, "valorTotal": 168720.30 },
+      { "descricao": "Rota 05", "quantidade": 28980, "unidade": "km", "valorUnitario": 6.38, "valorTotal": 184892.40 },
+      ... (mais 8 rotas)
+    ]
+  },
+  "aditamentos": [
+    {
+      "numero": 1, "data": "2025-03-05", "tipo": "misto",
+      "novaDataVencimento": "2026-03-05", "novoValorTotal": 1777314.00,
+      "percentualReajuste": 4.56, "indiceReajuste": "IPCA",
+      "observacoes": "Reajuste IPCA 4,56% e prorrogação por 12 meses",
+      "itensResultantes": [10 rotas com valores reajustados]
+    },
+    {
+      "numero": 2, "data": "2025-05-09", "tipo": "acrescimo",
+      "novoValorTotal": 2144297.40,
+      "observacoes": "Acréscimo de Rota 04, Rota 08 e Rota 13",
+      "itensResultantes": [13 rotas]
+    },
+    {
+      "numero": 3, "data": "2026-03-05", "tipo": "misto",
+      "novaDataVencimento": "2027-03-05", "novoValorTotal": 2237455.50,
+      "percentualReajuste": 4.44, "indiceReajuste": "IPCA",
+      "observacoes": "Reajuste IPCA 4,44% e prorrogação por 12 meses",
+      "itensResultantes": [13 rotas com valores reajustados]
+    }
+  ]
+}
